e4c212e7b8430c78f37b91b83be3132f330ad922
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-06-13 09:29:16 -0300

    subindo fotos perfil

--- system/js.js
+++ system/js.js
@@ -10,33 +10,43 @@ function pontos()
 			<table>
 
             <tr>
+            <td> <img src="../images/eu.png" src="perfil" class="perfil"> </td>
             <td class="nome">Ruanh</td> <td>6 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/brener.png" class="perfil"> </td>
             <td class="nome">Brener</td> <td>6 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/pedro.jpg" alt="perfil" class="perfil"> </td>
             <td class="nome">Pedro</td> <td>6 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/pickles.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Mr Pickles</td> <td>5 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/marcos.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Relâmpago</td> <td>5 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/semfoto.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Ademiro</td> <td>4 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/semfoto.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Jean</td> <td>4 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/tony.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Tony</td> <td>0 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/semfoto.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Victor</td> <td>0 pontos</td>
             </tr>
             <tr>
+            <td> <img src="../images/kenny.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Kennyd</td> <td>0 pontos</td>
             </tr>
             

b75e3a60fca60e25feccfea3287541b56276c555
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-06-16 12:04:08 -0300

    adicionando assistir

--- system/js.js
+++ system/js.js
@@ -67,9 +67,107 @@ function reload()
 function topo()
 {
     window.scrollTo(0, 0)
+
 }
 
-function jogos()
+function assistir()
 {
-    window.alert("em breve")
+    let a = window.document.getElementById("youtube")
+    a.innerHTML = `
+    
+    <div class="assistir">
+    <p><button class="assistir" onclick="fechar()"> Fechar </button></p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ysFOizDaozk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+
+    `
 }
+function fechar()
+{
+    let a = window.document.getElementById("youtube")
+    a.innerHTML = `
+    
+    <button class="assistir" onclick="assistir()">Assistir</button>
+
+
+    `
+}
+
+function assistir2()
+{
+    let a = window.document.getElementById("youtube2")
+    a.innerHTML = `
+    
+    <div class="assistir">
+    <p><button class="assistir" onclick="fechar2()"> Fechar </button></p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/FQceQNDrCTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+
+    `
+}
+function fechar2()
+{
+    let a = window.document.getElementById("youtube2")
+    a.innerHTML = `
+    
+    <button class="assistir" onclick="assistir2()">Assistir</button>
+
+
+    `
+}
+
+function assistir3()
+{
+    let a = window.document.getElementById("youtube3")
+    a.innerHTML = `
+    
+    <div class="assistir">
+    <p><button class="assistir" onclick="fechar3()"> Fechar </button></p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/sL3C2ROqWVo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+
+    `
+}
+function fechar3()
+{
+    let a = window.document.getElementById("youtube3")
+    a.innerHTML = `
+    
+    <button class="assistir" onclick="assistir3()">Assistir</button>
+
+
+    `
+}
+
+function assistir4()
+{
+    let a = window.document.getElementById("youtube4")
+    a.innerHTML = `
+    
+    <div class="assistir">
+    <p><button class="assistir" onclick="fechar4()"> Fechar </button></p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/fmJo71R8_G4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+
+
+    `
+}
+function fechar4()
+{
+    let a = window.document.getElementById("youtube4")
+    a.innerHTML = `
+    
+    <button class="assistir" onclick="assistir4()">Assistir</button>
+
+
+    `
+}
+
+
+
+
+
+

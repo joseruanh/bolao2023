e662e2b311e723c6834f239486021172ab398a72
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-06-27 09:00:52 -0300

    subindo rodadas 5 e 6

--- system/js.js
+++ system/js.js
@@ -10,41 +10,50 @@ function pontos()
 			<table>
 
             <tr>
-            <td> <img src="images/eu.png" src="perfil" class="perfil"> </td>
-            <td class="nome">Ruanh</td> <td>6 pontos</td>
-            </tr>
-            <tr>
-            <td> <img src="images/brener.png" class="perfil"> </td>
-            <td class="nome">Brener</td> <td>6 pontos</td>
-            </tr>
-            <tr>
             <td> <img src="images/pedro.jpg" alt="perfil" class="perfil"> </td>
-            <td class="nome">Pedro</td> <td>6 pontos</td>
+            <td class="nome">Pedro</td> <td>12 pontos</td>
             </tr>
+
             <tr>
-            <td> <img src="images/pickles.png" alt="perfil" class="perfil"> </td>
-            <td class="nome">Mr Pickles</td> <td>5 pontos</td>
+            <td> <img src="images/eu.png" src="perfil" class="perfil"> </td>
+            <td class="nome">Ruanh</td> <td>10 pontos</td>
             </tr>
+            
             <tr>
             <td> <img src="images/marcos.png" alt="perfil" class="perfil"> </td>
-            <td class="nome">Relâmpago</td> <td>5 pontos</td>
+            <td class="nome">Relâmpago</td> <td>9 pontos</td>
             </tr>
+
+            <tr>
+            <td> <img src="images/brener.png" class="perfil"> </td>
+            <td class="nome">Brener</td> <td>8 pontos</td>
+            </tr>
+          
+            <tr>
+            <td> <img src="images/pickles.png" alt="perfil" class="perfil"> </td>
+            <td class="nome">Mr Pickles</td> <td>8 pontos</td>
+            </tr>
+
             <tr>
             <td> <img src="images/semfoto.png" alt="perfil" class="perfil"> </td>
-            <td class="nome">Ademiro</td> <td>4 pontos</td>
+            <td class="nome">Ademiro</td> <td>8 pontos</td>
             </tr>
+
             <tr>
             <td> <img src="images/semfoto.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Jean</td> <td>4 pontos</td>
             </tr>
+
             <tr>
             <td> <img src="images/tony.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Tony</td> <td>0 pontos</td>
             </tr>
+
             <tr>
             <td> <img src="images/semfoto.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Victor</td> <td>0 pontos</td>
             </tr>
+            
             <tr>
             <td> <img src="images/kenny.png" alt="perfil" class="perfil"> </td>
             <td class="nome">Kennyd</td> <td>0 pontos</td>

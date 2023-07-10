1f042d071cb6fc6dffe4f6cde85b59696a2fadd4
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-07-10 10:29:52 -0300

    atualizando classificação

--- system/js.js
+++ system/js.js
@@ -10,36 +10,34 @@ function pontos()
 			<table>
 
             <tr>
-            <td> <img src="images/pedro.jpg" alt="perfil" class="perfil"> </td>
-            <td class="nome">Pedro</td> <td>20 pontos</td>
+            <td> <img src="images/eu.png" src="perfil" class="perfil"> </td>
+            <td class="nome">Ruanh</td> <td>35 pontos</td>
             </tr>
 
             <tr>
-            <td> <img src="images/eu.png" src="perfil" class="perfil"> </td>
-            <td class="nome">Ruanh</td> <td>18 pontos</td>
-            </tr>
-            
-            <tr>
-            <td> <img src="images/marcos.png" alt="perfil" class="perfil"> </td>
-            <td class="nome">Relâmpago</td> <td>18 pontos</td>
+            <td> <img src="images/pedro.jpg" alt="perfil" class="perfil"> </td>
+            <td class="nome">Pedro</td> <td>35 pontos</td>
             </tr>
 
             <tr>
             <td> <img src="images/ademiro.enc" alt="perfil" class="perfil"> </td>
-            <td class="nome">Ademiro</td> <td>17 pontos</td>
+            <td class="nome">Ademiro</td> <td>33 pontos</td>
+            </tr>
+
+            <tr>
+            <td> <img src="images/pickles.png" alt="perfil" class="perfil"> </td>
+            <td class="nome">Mr Pickles</td> <td>33 pontos</td>
             </tr>
 
             <tr>
             <td> <img src="images/brener.enc" class="perfil"> </td>
-            <td class="nome">Brener</td> <td>15 pontos</td>
-            </tr>
-          
-            <tr>
-            <td> <img src="images/pickles.png" alt="perfil" class="perfil"> </td>
-            <td class="nome">Mr Pickles</td> <td>15 pontos</td>
-            </tr>
-
+            <td class="nome">Brener</td> <td>26 pontos</td>
+            </tr> 
             
+            <tr>
+            <td> <img src="images/marcos.png" alt="perfil" class="perfil"> </td>
+            <td class="nome">Relâmpago</td> <td>18 pontos</td>
+            </tr>           
 
             <tr>
             <td> <img src="images/semfoto.png" alt="perfil" class="perfil"> </td>
